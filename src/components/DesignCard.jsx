2dfeab20c578c5ee8acbc10b0a71bf7054c05ab2
Fabian Montoya <montoyafabian28.fmfm@gmail.com>
2023-08-27 20:49:52 -0600

    Desing card height is fixed

--- src/components/DesignCard.jsx
+++ src/components/DesignCard.jsx
@@ -2,12 +2,12 @@
 export function DesignCard ({ design }) {
 
   return (
-    <div className="">
-      <a className="hover:cursor-pointer" href={`/shots/${design.id}`}>
-        <div className="w-full">
+    <a className="hover:cursor-pointer" href={`/shots/${design.id}`}>
+      <div className="h-[250px]">
+        <div className="w-full h-full">
           <img src={design.src} 
             alt={design.title} 
-            className=" w-full rounded-xl" 
+            className="object-fill h-full w-full rounded-xl" 
             />
         </div>
         <div className="flex font-Gilroy-Light gap-3 items-center justify-between text-xs mt-2 px-3">
@@ -15,7 +15,7 @@ export function DesignCard ({ design }) {
           <p>{design.likes} L</p>
           <p>{design.views} V</p>
         </div>
-      </a>
-    </div>
+      </div>
+    </a>
   )
 }
